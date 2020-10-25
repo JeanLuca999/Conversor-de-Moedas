2ccefa2625273529765242733d7411da04cec2bc
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2020-10-25 16:33:46 -0300

    um único arquivo para o js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+//funções do menu
+const botaoMenu = document.getElementsByClassName('botao-menu')[0]
+const menu = document.getElementsByClassName('menu')[0]
+let menuAperto = false
+
+botaoMenu.addEventListener('click', () => {
+    if (menuAperto == false) {
+        menu.style.transform = 'translateX(0%)'
+        menuAperto = true
+    } else if (menuAperto == true) {
+        menu.style.transform = 'translateX(-100%)'
+        menuAperto = false
+    }
+})
+
+//funções para usar a api
 const btnConverter = document.getElementById('converter')
 const resultado = document.getElementById('resultado')
 let condigoMoeda
